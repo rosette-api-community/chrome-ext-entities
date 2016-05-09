176659af557c1897f7b5eec32a5d7d43887c3bf6
SamHausmann <hausmann.sam@gmail.com>
2016-05-09 13:10:44 -0400

    API key persists in input after saving

--- options.js
+++ options.js
@@ -1,4 +1,14 @@
 // Saves options to chrome.storage.local.
+
+function saveKey() {
+	chrome.storage.local.get('rosetteKey', function(result){
+		if(result.rosetteKey != undefined)
+        document.getElementById('user-key').value = result.rosetteKey;
+	});
+}
+
+window.onload = saveKey();
+
 function handleOptions() {
   var entryField = document.getElementById('user-key');
   chrome.storage.local.set({'rosetteKey': entryField.value}, function() {
